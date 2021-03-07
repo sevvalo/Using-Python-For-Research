86da658c40f553d2568b49abf258afe4cf1dbdfe
Şevval Özdemir <76812049+sevvalo@users.noreply.github.com>
2021-03-07 17:21:49 +0300

    Add files via upload

---
+++ HW4-3.py
@@ -0,0 +1,81 @@
+from collections import Counter
+import numpy as np
+
+def marginal_prob(chars):
+    new = {}
+    for value in chars.values():
+        new[value] = 0
+    for key in new:
+        new[key] = key / len(new)
+    return new
+        
+        
+def chance_homophily(chars):
+    a = Counter(chars.values())
+    c = 0
+    for i in a.values():
+        c+= (i/len(chars))**2
+    return c
+favorite_colors = {
+    "ankit":  "red",
+    "xiaoyu": "blue",
+    "mary":   "blue"
+}
+
+color_homophily = chance_homophily(favorite_colors)
+
+import pandas as pd
+
+df  = pd.read_csv("https://courses.edx.org/asset-v1:HarvardX+PH526x+2T2019+type@asset+block@individual_characteristics.csv", low_memory=False, index_col=0)
+df1 = df[df.village == 1]
+df2 = df[df.village == 2]
+
+sex1      = dict(zip(df1['pid'],df1['resp_gend']))
+caste1 = dict(zip(df1['pid'],df1['caste']))
+religion1 = dict(zip(df1['pid'],df1['religion']))
+
+sex2      = dict(zip(df2['pid'],df2['resp_gend']))
+caste2 = dict(zip(df2['pid'],df2['caste']))
+religion2 = dict(zip(df2['pid'],df2['religion']))
+
+import networkx as nx
+
+def homophily(G, chars, IDs):
+    """
+    Given a network G, a dict of characteristics chars for node IDs,
+    and dict of node IDs for each node in the network,
+    find the homophily of the network.
+    """
+    num_same_ties = 0
+    num_ties = 0
+    for n1, n2 in G.edges():
+        if IDs[n1] in chars and IDs[n2] in chars:
+            if G.has_edge(n1, n2):
+                num_ties += 1
+                if chars[IDs[n1]] == chars[IDs[n2]]:
+                    num_same_ties += 1
+    return (num_same_ties / num_ties)    
+
+
+
+data_filepath1 = "https://courses.edx.org/asset-v1:HarvardX+PH526x+2T2019+type@asset+block@key_vilno_1.csv"
+data_filepath2 = "https://courses.edx.org/asset-v1:HarvardX+PH526x+2T2019+type@asset+block@key_vilno_2.csv"
+
+import networkx as nx
+A1 = np.array(pd.read_csv("https://courses.edx.org/asset-v1:HarvardX+PH526x+2T2019+type@asset+block@adj_allVillageRelationships_vilno1.csv", index_col=0))
+A2 = np.array(pd.read_csv("https://courses.edx.org/asset-v1:HarvardX+PH526x+2T2019+type@asset+block@adj_allVillageRelationships_vilno2.csv", index_col=0))
+G1 = nx.to_networkx_graph(A1)
+G2 = nx.to_networkx_graph(A2)
+
+pid1 = pd.read_csv(data_filepath1, dtype=int)['0'].to_dict()
+pid2 = pd.read_csv(data_filepath2, dtype=int)['0'].to_dict()
+
+
+
+
+
+
+
+
+
+
